070b8e82c4400cda4da5b803f2cf59fca8c8fc7c
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-07-27 13:07:34 +0300

    Setup the Navigation.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Missions from './components/Missions';
+import Navigation from './components/Navigation';
+import MyProfile from './components/MyProfile';
+
 function App() {
   return (
     <div className="App">
-      hello
+      <Navigation />
+      <Routes>
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
